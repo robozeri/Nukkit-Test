7a037350c7736448a92ab32539f76ec23959e8ea
robozeri <robozerirobot@gmail.com>
2015-11-21 14:33:52 +0100

    Update main.java

--- src/Nukkit-Test/java/robozeri/Nukkit-Test/main.java
+++ src/Nukkit-Test/java/robozeri/Nukkit-Test/main.java
@@ -1,1 +1,25 @@
+package robozeri.Nukkit-Test;
+
+import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.utils.TextFormat;
+
+import java.util.Objects;
+
+public class main extends PluginBase implements Listener {
+  
+  public void onEnable{
+    
+    this.getLogger.warning(TextFormat.GREEN + "Plugin test is enable");
+    this.getServer().getPluginManager().registerEvents(this, this);
+    String name = this.getLogger.getName();
+    this.getLogger.info(name);
+  }
+  public void onDisable{
+    
+    this.getlogger(TextFormat.GREEN + "Plugin test is disable");
+  }
+  
+  
+}
+
 
